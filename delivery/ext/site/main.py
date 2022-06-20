1e1cb97ecbc29dc9c89abd0808a73d0a6651d0a8
Igor Candido Rodrigues <igorcr39@gmail.com>
2022-06-20 06:58:33 -0300

    start using templates and flask_debugtoolbar

--- delivery/ext/site/main.py
+++ delivery/ext/site/main.py
@@ -1,6 +1,15 @@
-from flask import Blueprint
+from flask import Blueprint, render_template, current_app
 
 bp = Blueprint('site', __name__)
 @bp.route('/')
 def index():
-    return 'hello blueprint'
+    current_app.logger.debug('jsbdvisb')
+    return render_template('index.html')
+
+@bp.route('/sobre')
+def about():
+    return render_template('about.html')
+
+@bp.route('/restaurantes')
+def restaurants():
+    return render_template('restaurants.html')
