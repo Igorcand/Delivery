1171e82652e8bccf06753d7df1e0743ca62f724a
Igor Candido Rodrigues <igorcr39@gmail.com>
2022-06-23 06:47:20 -0300

    add some function to cli

--- delivery/ext/cli/__init__.py
+++ delivery/ext/cli/__init__.py
@@ -1,6 +1,7 @@
 from email.policy import default
 from delivery.ext.db import db
-from delivery.ext.site import models 
+from delivery.ext.auth.models import User
+from delivery.ext.db import models 
 import click
 
 def init_app(app):
@@ -16,14 +17,15 @@ def init_app(app):
 
     @app.cli.command()
     def listar_usuarios():
-        click.echo( 'lista de usuarios')
+        users = User.query.all()
+        click.echo( f'lista de usuarios: {users}')
 
     @app.cli.command()
     @click.option('--email', '-e')
     @click.option('--passwd', '-p')
     @click.option('--admin', '-a', is_flag=True, default=False)
     def add_user(email, passwd, admin):
-        user = models.User(
+        user = User(
             email=email,
             passwd=passwd,
             admin=admin
