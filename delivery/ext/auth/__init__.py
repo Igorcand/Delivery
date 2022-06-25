c3390d1ebff4f0cb0788458397812d581767ac72
Igor Candido Rodrigues <igorcr39@gmail.com>
2022-06-25 07:27:48 -0300

    some modifications to use admin with other tables

--- delivery/ext/auth/__init__.py
+++ delivery/ext/auth/__init__.py
@@ -1,6 +1,15 @@
 from delivery.ext.auth import models
 from delivery.ext.auth.commands import list_users, add_user
 
+from delivery.ext.db import db 
+from delivery.ext.auth.admin import UserAdmin
+from delivery.ext.admin import admin
+
+from delivery.ext.auth.models import User
+
 def init_app(app):
     app.cli.command()(list_users)
     app.cli.command()(add_user)
+
+    #pra adicionar uma view nos passamos a classe modelo da view, com os parametros do modelo da tabela e o banco de dados
+    admin.add_view(UserAdmin(User, db.session))
