9a0b9c45470501cc3f76cb99c6c1c7815739bab6
Igor Candido Rodrigues <igorcr39@gmail.com>
2022-06-25 07:28:04 -0300

    configuration

--- delivery/ext/config/__init__.py
+++ delivery/ext/config/__init__.py
@@ -2,8 +2,11 @@ def init_app(app):
     app.debug = True
     app.config['SECRET_KEY'] = "abacate01"
     app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///delivery.db"
+    app.config['FLASK_ADMIN_SWATCH'] = 'cerulean'
 
 
     if app.debug:
         app.config['DEBUG_TB_TEMPLATE_EDITOR_ENABLED'] = True
         app.config['DEBUG_TB_PROFILER_ENABLED'] = True
+        app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+
