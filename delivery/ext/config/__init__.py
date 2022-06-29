27d4789aa8d1efc45d647299b516d22d94f8f89b
Igor Candido Rodrigues <igorcr39@gmail.com>
2022-06-29 06:14:00 -0300

    change  configurations to daynaconf

--- delivery/ext/config/__init__.py
+++ delivery/ext/config/__init__.py
@@ -1,12 +1,6 @@
+from dynaconf import FlaskDynaconf
+
 def init_app(app):
-    app.debug = True
-    app.config['SECRET_KEY'] = "abacate01"
-    app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///delivery.db"
-    app.config['FLASK_ADMIN_SWATCH'] = 'cerulean'
-
-
-    if app.debug:
-        app.config['DEBUG_TB_TEMPLATE_EDITOR_ENABLED'] = True
-        app.config['DEBUG_TB_PROFILER_ENABLED'] = True
-        app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+    FlaskDynaconf(app)
+    app.config.load_extensions('EXTENSIONS')
 
