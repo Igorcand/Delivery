e5b89fae96ac6286c472102e5aef0e3f7887baf9
Igor Candido Rodrigues <igorcr39@gmail.com>
2022-07-08 06:31:14 -0300

    include route /cadastro

--- delivery/ext/site/main.py
+++ delivery/ext/site/main.py
@@ -1,4 +1,8 @@
-from flask import Blueprint, render_template, current_app
+from flask import Blueprint, render_template, current_app, redirect, request
+from delivery.ext.auth.form import UserForm
+from delivery.ext.auth.models import User
+from delivery.ext.auth.controller import create_user, save_user_foto
+
 
 bp = Blueprint('site', __name__)
 @bp.route('/')
@@ -13,3 +17,17 @@ def about():
 @bp.route('/restaurantes')
 def restaurants():
     return render_template('restaurants.html')
+
+@bp.route('/cadastro', methods=['GET','POST'])
+def signup():
+    form = UserForm()
+    if form.validate_on_submit():
+        create_user(email=form.email.data,password=form.password.data)
+        foto = request.files.get('foto')
+        if foto:
+            save_user_foto(foto.filename, foto)
+        
+        return redirect('/')
+
+
+    return render_template('userform.html', form=form)
